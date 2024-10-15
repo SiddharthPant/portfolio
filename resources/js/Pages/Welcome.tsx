f05699457e5d9ae91b530c14cc9d5e3689062d3f
SiddharthPant <spsiddharthpant@gmail.com>
2024-10-15 15:55:04 +0530

    Remove unnecessary info

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -357,7 +357,7 @@ export default function Welcome({
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
+                            Coming Soon
                         </footer>
                     </div>
                 </div>
