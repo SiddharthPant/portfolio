a760be4875d1717b3eb1f5f42df30fd20aa085e0
SiddharthPant <spsiddharthpant@gmail.com>
2024-10-17 13:42:28 +0530

    Add pagination and post type definitions

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,4 +1,4 @@
-import {Config} from 'ziggy-js';
+import { Config } from "ziggy-js";
 
 export interface User {
     id: number;
@@ -14,4 +14,40 @@ export type PageProps<
         user: User;
     };
     ziggy: Config & { location: string };
+};
+
+export type Paginate<T> = {
+    current_page: number;
+    data: T[];
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: {
+        url: string | null;
+        label: string;
+        active: boolean;
+    }[];
+    next_page_url: string | null;
+};
+
+export type Tag = {
+    id: number;
+    name: string;
+};
+
+export type Post = {
+    id: number;
+    title: string;
+    body?: string;
+    html?: string;
+    published_at: string | null;
+    likes_count: number;
+    dislikes_count: number;
+    stripped_content: string;
+    created_at: string;
+    updated_at: string;
+
+    // Relationships
+    tags: Tag[];
 };
