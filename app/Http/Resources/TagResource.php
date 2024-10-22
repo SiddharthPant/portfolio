b897b07097dc262e6a689e0975b02acb695d4ef3
SiddharthPant <spsiddharthpant@gmail.com>
2024-10-22 11:31:35 +0530

    Remove timestamps from tag resource

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -14,10 +14,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'name' => $this->name,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at, //
         ];
     }
 }
